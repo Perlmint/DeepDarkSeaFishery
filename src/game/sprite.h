1cd3006cb5d01b154719de14a85c26a0d333cef8
zzapcoder <zzapcoder@gmail.com>
2016-06-08 14:33:54 +0900

    Add missing header include

--- src/game/sprite.h
+++ src/game/sprite.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <memory>
 #include <cstdint>
+#include <string>
 #include "util.h"
 
 struct SDL_Renderer;
--- src/win/main_win.cpp
+++ src/win/main_win.cpp
@@ -9,7 +9,7 @@ bool initWindow(SDL_Window *window)
   SDL_VERSION(&info.version);
   if (SDL_GetWindowWMInfo(window, &info))
   {
-    HWND hwnd = info.window;
+    HWND hwnd = info.info.win.window;
     SetWindowLong(hwnd, GWL_EXSTYLE, GetWindowLong(hwnd, GWL_EXSTYLE) |WS_EX_LAYERED);
     SetLayeredWindowAttributes(hwnd, RGB(0,0,0), (255 * 70)/100, LWA_COLORKEY);
   }
